99915cc0b56e1a8a35f683b97ffd06c747f86632
aprikryl <aprikryl@redhat.com>
2023-11-27 18:49:46 +0100

    Fixed image similarity processing

--- test_display_image.py
+++ test_display_image.py
@@ -1,31 +1,47 @@
-import psycopg2
-from PIL import Image
-from io import BytesIO
 import numpy as np
+from keras.applications.vgg16 import VGG16, preprocess_input
+from keras.preprocessing import image
+from sklearn.metrics.pairwise import cosine_similarity
 
-# Connect to the PostgreSQL database
-conn_params = {
-    'dbname': 'image_similarity',
-    'user': 'postgres',
-    'password': 'postgres',
-    'host': '127.0.0.1'
-}
 
-conn = psycopg2.connect(**conn_params)
-cur = conn.cursor()
+# Function to preprocess and get embedding from an image
+def get_embedding_from_image(image_array):
+    # Use a pre-trained model (VGG16 in this case) for feature extraction
+    model = VGG16(weights='imagenet', include_top=False, input_shape=(32, 32, 3))
 
-# Retrieve the image from the database
-cur.execute("SELECT image_data FROM cifar_images LIMIT 1")
-record = cur.fetchone()
+    # Resize the image to the target size (32x32 pixels)
+    img_array = image.img_to_array(image_array)
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array = preprocess_input(img_array)
 
-# Convert bytea to NumPy array
-image_np_array = np.frombuffer(record[0], dtype=np.uint8)
+    # Get the embeddings
+    embeddings = model.predict(img_array)
 
-# Convert NumPy array to PIL Image
-image = Image.fromarray(image_np_array)
+    # Flatten the embeddings to a 1D array
+    embedding = embeddings.flatten()
 
-# Display or process the image as needed
-image.show()
+    return embedding
 
-cur.close()
-conn.close()
+# Replace 'path_to_cifar_image.png' with the path to your CIFAR-10 image
+cifar_image_ship = 'cifar10/test/ship/ship_0001.png'
+cifar_image_deer = 'cifar10/test/ship/ship_0002.png'
+# Load and preprocess the image
+img_ship = image.load_img(cifar_image_ship, target_size=(32, 32))
+img_deer = image.load_img(cifar_image_deer, target_size=(32, 32))
+img_array_ship = image.img_to_array(img_ship)
+img_array_deer = image.img_to_array(img_deer)
+
+# Get the embedding for the image
+embedding_ship = get_embedding_from_image(img_array_ship)
+embedding_deer = get_embedding_from_image(img_array_deer)
+
+# Now, 'embedding' contains the feature representation of the image
+#print(embedding_ship)
+#print(embedding_deer)
+
+embedding1_2d = embedding_ship.reshape(1, -1)
+embedding2_2d = embedding_deer.reshape(1, -1)
+
+similarity_score = cosine_similarity(embedding2_2d, embedding1_2d)[0, 0]
+
+print(f"Cosine Similarity: {similarity_score}")
