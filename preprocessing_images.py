1ed9ca5e4cc0966bc960ae166cbb178e1527d962
aprikryl <aprikryl@redhat.com>
2023-11-28 13:42:25 +0100

    Fixed bad images saving and processing into PostgreSQL

--- preprocessing_images.py
+++ preprocessing_images.py
@@ -2,6 +2,7 @@ import numpy as np
 import psycopg2
 from PIL import Image
 from pathlib import Path
+from datetime import datetime
 
 def preprocess_png(image_path):
     # Open and load the PNG image
@@ -19,22 +20,48 @@ def preprocess_png(image_path):
 
     return img_array
 
+def numpy_array_to_bytea(image_np_array):
+    # Ensure the pixel values are in the correct range [0, 255]
+    image_np_array_scaled = np.clip(image_np_array * 255, 0, 255).astype(np.uint8)
+
+    # Convert the NumPy array to bytes and then to BYTEA
+    bytea_data = psycopg2.Binary(image_np_array_scaled.tobytes())
+
+    return bytea_data
+
+def check_inserted_data(conn, cur):
+    try:
+        # Retrieve the saved data from the database
+        cur.execute("SELECT image_data FROM cifar_images_dimension_test WHERE image_category = 'automobile' AND image_type = 'test';")
+        serialized_array = cur.fetchone()[0]
+        
+        # Deserialize the data to a NumPy array
+        preprocessed_image = np.frombuffer(bytes(serialized_array), dtype=np.uint8)
+        preprocessed_image = preprocessed_image.reshape((32, 32, 3))
+
+        # Check if the dimensions match
+        if preprocessed_image.shape != (32, 32, 3):
+            print("Error: Saved image data has unexpected dimensions.")
+        else:
+            print("Saved image data has the expected dimensions.")
+        
+    except Exception as e:
+        print(f"Error checking data: {e}")
+
+# Connect to the PostgreSQL database
 conn_params = {
     'dbname': 'image_similarity',
     'user': 'postgres',
     'password': 'postgres',
     'host': '127.0.0.1'
 }
-
-# Connect to the PostgreSQL database
 conn = psycopg2.connect(**conn_params)
 cur = conn.cursor()
 
-directory = Path('cifar10/test/truck')
+directory = Path('cifar10/test/automobile')
 image_files = list(directory.glob('*.png'))
 
-# Get the number of image files in the directory
-number_of_images = len(image_files)
+current_timestamp = datetime.now()
 
 # Loop through each file in the directory
 for file_path in image_files:
@@ -42,14 +69,18 @@ for file_path in image_files:
     preprocessed_image = preprocess_png(str(file_path))
 
     # Convert NumPy array to BYTEA suitable for PostgreSQL
-    serialized_array = psycopg2.Binary(preprocessed_image.tobytes())
+    serialized_array = numpy_array_to_bytea(preprocessed_image)
 
     try:
         cur.execute(
-            f"INSERT INTO cifar_images (image_name, image_data, image_category, image_type) VALUES (%s, %s, %s, %s)",
-            (file_path.name, serialized_array, 'truck', 'test'))
+            f"INSERT INTO cifar_images_dimension_test (image_name, image_data, created_at, image_category, image_type) VALUES (%s, %s, %s, %s, %s)",
+            (file_path.name, serialized_array, current_timestamp, 'automobile', 'test'))
         conn.commit()
         print("Record inserted successfully.")
+
+        # Check the saved data after each insert
+        check_inserted_data(conn, cur)
+
     except Exception as e:
         print(f"Error inserting data: {e}")
         conn.rollback()
