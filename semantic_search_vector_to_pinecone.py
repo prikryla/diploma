924c060af29ce64e8a9e63eb6b834fbc5c6b1779
aprikryl <aprikryl@redhat.com>
2024-04-09 23:51:10 +0200

    Fixed Pinecone syntax for creating index and upserting vectors

--- semantic_search_vector_to_pinecone.py
+++ semantic_search_vector_to_pinecone.py
@@ -1,4 +1,4 @@
-import pinecone
+from pinecone import Pinecone, ServerlessSpec
 import pandas as pd
 from sentence_transformers import SentenceTransformer
 
@@ -9,14 +9,27 @@ df = pd.read_csv(csv_file_path, delimiter=';')
 # Load pre-trained Sentence Transformer model
 model = SentenceTransformer('paraphrase-MiniLM-L6-v2')
 
-# Connect to the Pinecone index
-pinecone.init(api_key="eeb95b42-5012-4301-854b-7d75ae9fd293", environment="gcp-starter")
-index_name = "semantic"  # Replace with your desired index name
+pc = Pinecone(
+    api_key="eeb95b42-5012-4301-854b-7d75ae9fd293"  # Replace with your API key
+)
 
-# Specify the correct dimension
-index_dimension = 384
-# pinecone.create_index(index_name, dimension=index_dimension)
-index = pinecone.Index(index_name)
+# Define the index parameters
+index_name = "clustering"  # Replace with your index name
+index_dimension = 384  # Assuming your Sentence Transformer model has an output dimension of 768
+
+# Check if the index already exists, if not, create it
+if index_name not in pc.list_indexes().names():
+    pc.create_index(
+        name=index_name,
+        dimension=index_dimension,
+        metric='cosine',  # You might want to adjust the metric based on your use case
+        spec=ServerlessSpec(
+            cloud='gcp',
+            region='us-central1'
+        )
+    )
+
+index = pc.Index(index_name)
 
 # Function to create embeddings and save to Pinecone
 def create_and_save_embeddings():
