0ad3ba51370955f6d5003c116fe90cd085835608
aprikryl <aprikryl@redhat.com>
2024-04-29 13:00:19 +0200

    Hide connection parameters to Postgres

--- create_db_sem_search.py
+++ create_db_sem_search.py
@@ -1,25 +1,31 @@
 import psycopg2
-from psycopg2 import sql
+import os
 
+from psycopg2 import sql
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 # Connection parameters
 conn_params = {
-    'dbname': 'diploma_2',
-    'user': 'postgres',
-    'password': 'postgres',
-    'host': '127.0.0.1'
+    'dbname': os.getenv('PG_DBNAME'),
+    'user': os.getenv('PG_USER'),
+    'password': os.getenv('PG_PASSWORD'),
+    'host': os.getenv('PG_HOST')
 }
+
 
 # Connect to the PostgreSQL server
 conn = psycopg2.connect(**conn_params)
 cursor = conn.cursor()
 
 create_table_query = """
-    CREATE TABLE diploma_semantic_search (
+    CREATE TABLE IF NOT EXISTS diploma_semantic_search (
         id SERIAL PRIMARY KEY,
         class_index VARCHAR,
         title VARCHAR,
         description TEXT,
-        embedding FLOAT[]
+        embedding BYTEA
     );
 """
 
